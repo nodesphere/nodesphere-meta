c86363776b3ef8aef3a313102c1175d051c510aa
Harlan T Wood <harlan@thegoldensun.com>
2014-10-11 15:49:06 -0700

    serve dist as public dir

---
+++ server.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+ROOT_PATH = path.resolve(__dirname, 'dist/');
+
+app.get('*', function (req, res) {
+	res.sendfile(ROOT_PATH + req.url);
+});
+
+port = process.env.PORT || 5555
+if (server.listen(port))
+  console.log("...listening up on " + port); 
+
